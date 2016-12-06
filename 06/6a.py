48380c1794101c8a6b99b2aea38c85f3b469be9a
adamb70 <adam.boaler@hotmail.com>
2016-12-06 10:13:13 +0000

    Day 6

---
+++ 06/6a.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+DATA = [[] for _ in range(8)]
+result = ""
+
+with open('6.txt', 'r') as file:
+    for line in file.readlines():
+        line = line.strip()
+        for i in range(8):
+            DATA[i].append(line[i])
+
+
+for col in DATA:
+    result += Counter(col).most_common()[0][0]
+
+print(result)
